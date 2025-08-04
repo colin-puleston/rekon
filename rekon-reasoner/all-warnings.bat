112963ef523ed1c9250bd2cef908254fec1f6ded
Colin-Puleston <colins.email@virgin.net>
2025-08-04 14:24:43 +0100

    Added warnings.bat and all-warnings.bat

---
+++ rekon-reasoner/all-warnings.bat
@@ -0,0 +1,5 @@
+call warnings outofscope-axioms 4
+call warnings outofscope-exprs 14
+call warnings substitutions 2
+call warnings incomplete 1
+
---
+++ rekon-reasoner/warnings.bat
@@ -0,0 +1,2 @@
+call test rekon.test.Run 0:0 REKON test-warnings test-%1 > WARNINGS-OUT-%1-%2-warnings
+
