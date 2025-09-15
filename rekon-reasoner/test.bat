db47773707f2454af38845b40c2ebb921d1c132a
Colin-Puleston <colins.email@virgin.net>
2025-09-15 16:41:42 +0100

    Updated test.bat

--- rekon-reasoner/test.bat
+++ rekon-reasoner/test.bat
@@ -1,1 +1,8 @@
-java -cp build/lib/*;build/classes -Xmx5000M -Xss200M -Djava.library.path=build/lib/ -Xrunhprof:cpu=samples -Drekon.multithread=true -Drekon.novalue-substitutions=true -Drekon.logging.warnings=true -Drekon.logging.progress=false %*
+java -cp build/lib/*;build/classes;test/config -Xmx5000M -Xss200M -Djava.library.path=build/lib/ -Xrunhprof:cpu=samples %*
+
+REM Possible command-line options - alternative to using "rekon.options" config file in "test/config" dir
+REM
+REM -Drekon.multithread=true
+REM -Drekon.novalue-substitutions=true
+REM -Drekon.logging.warnings=true
+REM -Drekon.logging.progress=false
